9f755fe8bdfe0ee2016ac2d6fec581064379388c
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-06-29 03:53:26 +0530

    ✨ feat: Create all the necessary routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { Navbar } from './Components/Navbar';
+import { Store } from './Pages/Store';
+import { ProductDetails } from './Pages/ProductDetails';
 
 function App() {
 
 
   return (
-    <h1>Hi</h1>
+    <React.Fragment>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Navigate to="/store" />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/store/product/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<h1>Cart</h1>} />
+        </Routes>
+      </Container>
+    </React.Fragment>
   )
 }
 
-export default App
+export default App;
